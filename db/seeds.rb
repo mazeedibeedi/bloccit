022a12275a417f0b3963d9334f028a9bdb76f177
Ahmed Almazidi <a.almazidi92@gmail.com>
2017-06-02 12:33:03 +0300

    Assignment 17 complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,6 +17,14 @@ posts = Post.all
     body: RandomData.random_paragraph
   )
 end
+Post.find_or_create_by!(
+  title: "This is a unique title",
+  body: "This is a unique body"
+)
+
+Comment.find_or_create_by!(
+  body: "This is also a unique body for a unique comment"
+)
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
