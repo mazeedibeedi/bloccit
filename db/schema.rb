ee29a08959e3a60c3cfcd2da423af70d46f82c91
Ahmed Almazidi <a.almazidi92@gmail.com>
2017-06-13 02:11:18 +0300

    Checkpoint 22 complete

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170527103424) do
+ActiveRecord::Schema.define(version: 20170612195958) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -24,9 +24,28 @@ ActiveRecord::Schema.define(version: 20170527103424) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
-    t.string   "body"
+    t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "topic_id"
+  end
+
+  add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
+
+  create_table "questions", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.boolean  "resolved"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "public",      default: true
+    t.text     "description"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
 end
